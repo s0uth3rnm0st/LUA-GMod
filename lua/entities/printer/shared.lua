7c324811d719756b8d3e08742f8da7b65abbe641
s0uth3rnm0st <47353542+s0uth3rnm0st@users.noreply.github.com>
2020-02-19 13:12:14 -0300

    Update shared.lua

--- lua/entities/printer/shared.lua
+++ lua/entities/printer/shared.lua
@@ -9,10 +9,10 @@ ENT.Spawnable = true
 function ENT:SetupDataTables()
 
 	self:NetworkVar("Int", 1 , "MoneyAmount")
-	//Stores an entity on the serverside of an entity and sends it to the client,
-	//the client can not change it or munipulate it, but it can see what it is.
-	//In the case of a printer, it's not on the clientside because if it's, the 
-	//client would be able to change it, that means, the user, not the server.
-	//Try to never trust the client then...
+	--Stores an entity on the serverside of an entity and sends it to the client,
+	--the client can not change it or munipulate it, but it can see what it is.
+	--In the case of a printer, it's not on the clientside because if it's, the 
+	--client would be able to change it, that means, the user, not the server.
+	--Try to never trust the client then...
 
 end
